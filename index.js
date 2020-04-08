2c4dd0203c62539f92e9c18a9b7587cc905a15b0
Oliver Dahl <oliverpdahl@gmail.com>
2020-04-08 17:30:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 // Write your code here!
+let newHeader = document.createElement('h1#victory');
+newHeader.innerHTML = 'Oliver is the champion';
+
 const main = document.querySelector('main#main');
 main.remove();
 
-let newHeader = document.createElement('h1#victory');
-newHeader.innerHTML = 'Oliver is the champion';
-element = document.createElement('div');
-document.body.appendChild(element);
-element.appendChild(newHeader);
-return newHeader;
+
+// element = document.createElement('div');
+// document.body.appendChild(element);
+// element.appendChild(newHeader);
+// return newHeader;
